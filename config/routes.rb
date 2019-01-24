b2bffb4c565f31d3fdef70b39c9f10655ed70745
roller2410 <roller2410@outlook.com>
2019-01-23 22:51:45 -0800

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.
-  root 'index#index'
+
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'index#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   get 'single_sms/index'
   get 'bulk_sms/index'
