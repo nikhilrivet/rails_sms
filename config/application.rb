1cc86cf8c6791c33c94d656a72c34cfea3cfb199
roller2410 <roller2410@outlook.com>
2019-01-24 00:05:22 -0800

    reset

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,26 @@ module SmsRivet
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_mailer.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+        address: "smtp.gmail.com",
+        enable_starttls_auto: true,
+        port: 587,
+        authentication: :plain,
+        user_name: ENV["MAIL_USERNAME"],
+        password: ENV["MAIL_PASSWORD"],
+        openssl_verify_mode: 'none'
+    }
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
   end
 end
+
+
