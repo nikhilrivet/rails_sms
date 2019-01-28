afb306f2d02665273067d9c51658ae9b9e170f6a
roller2410 <roller2410@outlook.com>
2019-01-28 03:55:04 -0800

    distribution

--- app/controllers/group_sms_controller.rb
+++ app/controllers/group_sms_controller.rb
@@ -16,39 +16,42 @@ class GroupSmsController < ApplicationController
     uri = URI('http://95.179.214.39:1401/send')
     dlr_url = 'https://rivetsms.herokuapp.com/delivery_receipt/get_dlr'
 
-=begin
-    @togroups.each do |item|
-      contacts = Contact.where(:group_id => item.to_i)
+    if params[:togroup]
+      @togroups.each do |item|
+        contacts = Contact.where(:group_id => item.to_i)
 
-      contacts.each do |contact|
-        params = { :username => 'foo', :password => 'bar',
-                   :to => contact.number, 'content' => message.encode("UTF-16BE"),:from => @sender ,
-                   :coding => 8,
-                   :dlr => 'yes', 'dlr-level' => 2, 'dlr-url' => dlr_url}
-        uri.query = URI.encode_www_form(params)
+        contacts.each do |contact|
+          params = { :username => 'foo', :password => 'bar',
+                     :to => contact.number, 'content' => message.encode("UTF-16BE"),:from => @sender ,
+                     :coding => 8,
+                     :dlr => 'yes', 'dlr-level' => 2, 'dlr-url' => dlr_url}
+          uri.query = URI.encode_www_form(params)
 
-        @response = Net::HTTP.get_response(uri)
-        @message_id = @response.body.from(9).to(-2)
-        @message = Message.create(phone: contact.number, sender: @sender, message: message, message_id: @message_id, message_status: 'PENDING')
+          @response = Net::HTTP.get_response(uri)
+          @message_id = @response.body.from(9).to(-2)
+          @message = Message.create(phone: contact.number, sender: @sender, message: message, message_id: @message_id, message_status: 'PENDING')
+        end
       end
     end
-=end
 
-    @todistributions.each do |item|
-      dcontacts =Dcontact.where(:distribution_id => item.to_i)
+    if params[:todistribution]
+      @todistributions.each do |item|
+        dcontacts =Dcontact.where(:distribution_id => item.to_i)
 
-      dcontacts.each do |dcontact|
-        params = { :username => 'foo', :password => 'bar',
-                   :to => dcontact.number, 'content' => message.encode("UTF-16BE"),:from => @sender ,
-                   :coding => 8,
-                   :dlr => 'yes', 'dlr-level' => 2, 'dlr-url' => dlr_url}
-        uri.query = URI.encode_www_form(params)
+        dcontacts.each do |dcontact|
+          params = { :username => 'foo', :password => 'bar',
+                     :to => dcontact.number, 'content' => message.encode("UTF-16BE"),:from => @sender ,
+                     :coding => 8,
+                     :dlr => 'yes', 'dlr-level' => 2, 'dlr-url' => dlr_url}
+          uri.query = URI.encode_www_form(params)
 
-        @response = Net::HTTP.get_response(uri)
-        @message_id = @response.body.from(9).to(-2)
-        @message = Message.create(phone: dcontact.number, sender: @sender, message: message, message_id: @message_id, message_status: 'PENDING')
+          @response = Net::HTTP.get_response(uri)
+          @message_id = @response.body.from(9).to(-2)
+          @message = Message.create(phone: dcontact.number, sender: @sender, message: message, message_id: @message_id, message_status: 'PENDING')
+        end
       end
     end
+
 
     flash[:success] = "Success: Message sent successfully."
     redirect_to group_sms_index_path, locals: {response: @response}
