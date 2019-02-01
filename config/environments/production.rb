e686d544f8911707d011392850e286b9fefee7f5
roller2410 <roller2410@outlook.com>
2019-02-01 13:16:16 -0800

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   config.assets.compile = true
 
   config.assets.debug = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
