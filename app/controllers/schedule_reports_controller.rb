60066fc744dc5b0382578bc9d59590fd7f58bf2b
roller2410 <roller2410@outlook.com>
2019-03-06 13:02:59 +0400

    update batch

--- app/controllers/schedule_reports_controller.rb
+++ app/controllers/schedule_reports_controller.rb
@@ -1,5 +1,5 @@
 class ScheduleReportsController < BaseController
   def index
-    @batchs = Schedule.where(:user_id => current_user.id).order('id DESC')
+    @batchs = Schedule.where(:user_id => current_user.id).where.not(schedule_time: [nil, ""]).order('id DESC')
   end
 end
