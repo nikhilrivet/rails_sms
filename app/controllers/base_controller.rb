fa67efe27ed285565e9e194e07e7500bc5c23ef3
roller2410 <roller2410@outlook.com>
2019-03-09 02:47:26 +0400

    base controller

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -5,12 +5,15 @@ class BaseController < ApplicationController
   def verify_user
     if current_user.nil?
       redirect_to(unauthenticated_root_path)
+      return
     end
     if current_user.admin?
       redirect_to(admin_root_path)
+      return
     end
     if current_user.reseller?
       redirect_to(reseller_root_path)
+      return
     end
 =begin
     (current_user.nil?) ? redirect_to(unauthenticated_root_path) : (redirect_to(admin_root_path) if current_user.admin?) : (redirect_to(reseller_root_path) if current_user.reseller?)
